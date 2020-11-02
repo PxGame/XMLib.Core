13b0522a4189d27578781d4ffe607ee65a094ea2
PeterXiang <565067150@qq.com>
2020-11-02 19:05:40 +0800

    添加主线程回调

--- Runtime/UnityApplication.cs
+++ Runtime/UnityApplication.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 namespace XMLib
@@ -20,10 +21,38 @@ namespace XMLib
         public Application app;
         public Action onDestroyed;
 
+        #region Main Thread
+
+        protected Queue<Action> actions = new Queue<Action>(32);
+
+        public void Run(Action callback)
+        {
+            lock (actions)
+            {
+                actions.Enqueue(callback);
+            }
+        }
+
+        protected void RunUpdate()
+        {
+            lock (actions)
+            {
+                while (actions.Count > 0)
+                {
+                    Action act = actions.Dequeue();
+                    act();
+                }
+            }
+        }
+
+        #endregion Main Thread
+
         #region Mono
 
         protected void Update()
         {
+            RunUpdate();
+
             app?.RunUpdate();
         }
 
